2a0ca6596450436d3946e65415a33e5effe688c9
Somesh Singh <somesh.singh1992@gmail.com>
2015-03-24 15:48:51 +0530

    version 2 s Please enter the commit message for your changes. Lines starting

--- builder.py
+++ builder.py
@@ -1,34 +1,38 @@
 import wire
-
+import tree
 def Builder(tr, primary_var):
 	# base case (to be written)
-        if isLiteral(tr, primary_var):
-                out = wire.wire()
-		out = tr
-		return out
+	if isLiteral(tr, primary_var):
+		return tr 
 	# recursive case
 	elif (tr.is_and() and (not tr.is_circuit())):
 #		temp_out = chr(ord(temp_out)+1) #the temporary output variable 
 		gate_str = "and_gate" 
 		in1 = Builder(tr.get_left_node(), primary_var)
 		in2 = Builder(tr.get_right_node(), primary_var)
-		out = gate_str + "(" + str(in1) + "," + str(in2) + "," + temp + "the_agenda)" 
-                print out
+                make_and_gate(in1, in2)
+		make_circuit_node(tr)
+	#	out = gate_str + "(" + str(in1) + "," + str(in2) + "," + temp + ",the_agenda)" 
+		
+         #       return out
 
         elif (tr.is_or() and (not tr.is_circuit())):
 		gate_str = "or_gate" 
 		in1 = Builder(tr.get_left_node(), primary_var)
 		in2 = Builder(tr.get_right_node(), primary_var)
-		out = gate_str + "(" + str(in1) + "," + str(in2) + "," + temp + ",the_agenda)" 
-                print out
+                make_or_gate(in1, in2)
+		make_circuit_node(tr)
+	#	out = gate_str + "(" + str(in1) + "," + str(in2) + "," + temp + ",the_agenda)" 
+        #        return out
 
-
+	return tr
 def isLiteral(node, primary_var):
 	for var in primary_var:
 		if node == var or node == (not var):
 			return True
 
 def main():
+	tr = tree()
 	primary_var = ['A', 'B', 'C', 'D']
         primary_var += ['OUT'] 
         S1 = ""
@@ -39,7 +43,7 @@ def main():
 		S1+= (str(var)+',')
         S5 = S + S1 + S4 + new_line
         print (S5) 
-#	Builder(tr, primary_var)
+	Builder(tr, primary_var)
 
 if __name__ == '__main__':
     main()
